216e0776e2ad47d64c64a55730b910ff72e765c2
Sangam Rai <raisangam77@gmail.com>
2023-11-16 22:08:19 +1030

    The First Commit

---
+++ SimplePythonProgram.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+# A simple program to calculate sum of two numbers
+a=int(input("Enter first number:"))
+
+print("Sum of the two numbers:",Sum)
+
+
+
+# In[ ]:
+
+
+
+
